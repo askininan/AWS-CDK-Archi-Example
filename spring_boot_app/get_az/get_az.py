f48a97c36469b78765394cbe41173abc1f755bda
askininan <askin.inan@outlook.com>
2022-02-04 22:18:50 +0300

    added an ALB and 2 scaling groups

--- spring_boot_app/get_az/get_az.py
+++ spring_boot_app/get_az/get_az.py
@@ -1,25 +1,15 @@
 import boto3
 
 ec2 = boto3.resource('ec2')
-
-# instances = ec2.instances.filter(
-#     Filters=[
-#         {'Name': 'instance-state-name', 'Values': ['running']}
-#     ]
-# )
-
-# for instance in instances:
-#    print(instance.id, instance.instance_type, instance.private_ip_address)
-
   
 output = {}
 
 for instance in ec2.instances.all():
-    for iface in instance.network_interfaces:
+    for interface in instance.network_interfaces:
         output[instance.id] = {
             'Instance ID': instance.id,
-            'Subnet ID': iface.subnet_id,
-            'AZ': iface.subnet.availability_zone
+            'Subnet ID': interface.subnet_id,
+            'AZ': interface.subnet.availability_zone
         }
 
 print(output)
--- spring_boot_app/spring_boot_app_stack.py
+++ spring_boot_app/spring_boot_app_stack.py
@@ -7,8 +7,10 @@ from aws_cdk import (
     Stack,
     aws_apigateway as apigw,
     aws_ec2 as ec2,
-    aws_autoscaling as autoscaling
+    aws_autoscaling as autoscaling,
+    aws_cloudwatch as cw,
 )
+
 from constructs import Construct
 
 # Define EC2 instance configurations
@@ -30,12 +32,25 @@ class SpringBootAppStack(Stack):
 
         # Deploy custom VPC
         vpc = ec2.Vpc(
-        self, "CMS_vpc",
+        self, "Spring_Boot_VPC",
         cidr="10.0.0.0/16"
         )
 
+        # Create ALB
+        alb = elb.ApplicationLoadBalancer(self, "myALB",
+                                          vpc=vpc,
+                                          internet_facing=True,
+                                          load_balancer_name="myALB"
+                                          )
+        alb.connections.allow_from_any_ipv4(
+            ec2.Port.tcp(80), "Internet access ALB 80")
+        listener = alb.add_listener("my80",
+                                    port=80,
+                                    open=True)
+        
+        
         # EC2 autoscaling group with one EC2 instance at start
-        self.auto_scaling_g = autoscaling.AutoScalingGroup(self, "APP ASG",
+        auto_scaling_g = autoscaling.AutoScalingGroup(self, "APP ASG",
                                                 vpc=vpc,
                                                 vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT),
                                                 instance_type=ec2.InstanceType(instance_type_identifier=ec2_type),
@@ -45,3 +60,42 @@ class SpringBootAppStack(Stack):
                                                 min_capacity=1,
                                                 max_capacity=2,
         )
+
+        # Second EC2 autoscaling group with one EC2 instance at start
+        auto_scaling_g2 = autoscaling.AutoScalingGroup(self, "APP ASG2",
+                                                vpc=vpc,
+                                                vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT),
+                                                instance_type=ec2.InstanceType(instance_type_identifier=ec2_type),
+                                                machine_image=linux_ami,
+                                                user_data=ec2.UserData.custom(user_data),
+                                                desired_capacity=1,
+                                                min_capacity=1,
+                                                max_capacity=2,
+        )
+
+
+        target_tracking_scaling_policy = autoscaling.TargetTrackingScalingPolicy(self, "TargetScalingPolicy",
+            auto_scaling_group=auto_scaling_g,
+            predefined_metric=autoscaling.PredefinedMetric.ASG_AVERAGE_CPU_UTILIZATION,
+            target_value=80,
+            cooldown=aws_cdk.Duration.minutes(20),
+            disable_scale_in=False,
+            estimated_instance_warmup=aws_cdk.Duration.minutes(20),
+        )
+
+        target_tracking_scaling_policy2 = autoscaling.TargetTrackingScalingPolicy(self, "TargetScalingPolicy2",
+            auto_scaling_group=auto_scaling_g2,
+            predefined_metric=autoscaling.PredefinedMetric.ASG_AVERAGE_CPU_UTILIZATION,
+            target_value=80,
+            cooldown=aws_cdk.Duration.minutes(20),
+            disable_scale_in=False,
+            estimated_instance_warmup=aws_cdk.Duration.minutes(20),
+        )
+
+        self.asg.connections.allow_from(alb, ec2.Port.tcp(80), "ALB access 80 port of EC2 in Autoscaling Group")
+        listener.add_targets("addTargetGroup1",
+                             port=80,
+                             targets=[auto_scaling_g]
+                             
+                             
+                             )
