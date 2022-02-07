df2666b139c8201beb06d1e97965b15cf2a96e7f
askininan <askin.inan@outlook.com>
2022-02-07 18:33:18 +0300

    Added script file creating codes in user data

--- spring_boot_app/get_az/get_az.py
+++ spring_boot_app/get_az/get_az.py
@@ -1,15 +1,22 @@
+#!./.venv/bin/python3
+
 import boto3
+    
+def main():
+    ec2 = boto3.resource('ec2')
+    
+    output = {}
 
-ec2 = boto3.resource('ec2')
-  
-output = {}
+    for instance in ec2.instances.all():
+        for interface in instance.network_interfaces:
+            output[instance.id] = {
+                'Instance ID': instance.id,
+                'Subnet ID': interface.subnet_id,
+                'AZ': interface.subnet.availability_zone
+            }
 
-for instance in ec2.instances.all():
-    for interface in instance.network_interfaces:
-        output[instance.id] = {
-            'Instance ID': instance.id,
-            'Subnet ID': interface.subnet_id,
-            'AZ': interface.subnet.availability_zone
-        }
+    print(output)
 
-print(output)
+
+if __name__ == '__main__':
+    main()
--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -3,3 +3,8 @@ sudo yum update -y
 sudo yum -y install httpd php
 sudo chkconfig httpd on
 sudo service httpd start
+
+echo "<h1>Hello World from $(hostname -f)</h1>"
+
+echo curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ > hello
+chmod +x hello
